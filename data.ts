a9e74a83b48ae09a57b5dfe5c49825fb9bf945d8
anmol-sainii <71484001+anmol-sainii@users.noreply.github.com>
2025-01-15 23:35:26 +1100

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,27 @@
+export var single = [
+    {
+      "name": "Completed",
+      "value": 1
+    },
+    {
+      "name": "Ready for Feedback",
+      "value": 2
+    },
+    {
+      "name": "Resubmit",
+      "value": 1
+    },
+    {
+      "name": "Fail",
+      "value": 2
+    },
+    {
+      "name": "Not Started",
+      "value": 5
+    },
+    {
+      "name": "Working On It",
+      "value": 5
+    }
+  ];
+  
